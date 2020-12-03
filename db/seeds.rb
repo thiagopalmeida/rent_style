fe1eba649c1bd793248e0c0a676f838f5aedaa53
Ana Paula Tavares Pontello Neves <anapaulapontello@gmail.com>
2020-12-03 16:24:29 -0300

    Melhorei a seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,34 +6,49 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+puts "Criando novos usuários:"
+
+10.times do
+  u = User.create(
+    email: Faker::Internet.email,
+    name: Faker::Name.name_with_middle,
+    address: Faker::Address.street_address,
+    birth_date: Faker::Date.birthday,
+    telephone: Faker::PhoneNumber.phone_number,
+    password: '123456'
+  )
+  puts "User created: #{u.id} | #{u.name} | #{u.address} | #{u.birth_date} | #{u.telephone}"
+  u.save
+end
+
 # puts "Apagando produtos anteriores..."
 # Product.destroy_all
 # puts "Produtos apagados!"
-# puts "Criando novos produtos de exemplo:"
+puts "Criando novos produtos de exemplo:"
 
-# 50.times do
-#   p = Product.create(
-#     description: Faker::Commerce.product_name,
-#     category: ["Categoria 1", "Categoria 2", "Categoria 3"].sample,
-#     subcategory: ["Subcategoria 1", "Subcategoria 2", "Subcategoria 3"].sample,
-#     brand: ["Marca 1", "Marca 2", "Marca 3"].sample,
-#     size: [rand(2..60), "PP", "P", "M", "G", "GG", "XG", "XXG", "Único"].sample,
-#     price: Faker::Commerce.price,
-#     user_id: rand(1..4)
-#   )
+50.times do
+  p = Product.create(
+    description: Faker::Commerce.product_name,
+    category: ["Categoria 1", "Categoria 2", "Categoria 3"].sample,
+    subcategory: ["Subcategoria 1", "Subcategoria 2", "Subcategoria 3"].sample,
+    brand: ["Marca 1", "Marca 2", "Marca 3"].sample,
+    size: [rand(2..60), "PP", "P", "M", "G", "GG", "XG", "XXG", "Único"].sample,
+    price: Faker::Commerce.price,
+    user_id: rand(1..10)
+  )
 
-#   puts "Product #{p.id} - #{p.description} created!"
-#   p.save
-# end
+  puts "Product #{p.id} - #{p.description} created!"
+  p.save
+end
 
-puts "Apagando transações anteriores..."
-Transaction.destroy_all
-puts "Transações apagadas!"
+# puts "Apagando transações anteriores..."
+# Transaction.destroy_all
+# puts "Transações apagadas!"
 puts "Criando novas transações de exemplo:"
 
 100.times do
   t = Transaction.create(
-    user_id: rand(1..4),
+    user_id: rand(1..10),
     product_id: rand(101..150),
     payment_method: ["Boleto", "Cartão de Crédito", "Transferência"].sample
   )
