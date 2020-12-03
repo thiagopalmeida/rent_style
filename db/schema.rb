f050a6d19299335cda841846545881092b6733bc
Millena Wiese <millenawiese@gmail.com>
2020-12-03 16:02:10 -0300

    home corrigida

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,6 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 2020_12_03_102219) do
-ActiveRecord::Schema.define(version: 2020_12_03_013742) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,6 +34,8 @@ ActiveRecord::Schema.define(version: 2020_12_03_013742) do
     t.string "checksum", null: false
     t.datetime "created_at", null: false
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
+
   create_table "products", force: :cascade do |t|
     t.string "category"
     t.string "subcategory"
