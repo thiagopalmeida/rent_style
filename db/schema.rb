5819cb24e6cc20c7e1368eb05086d10001e56949
thiago almeida <thiagopalmeida@gmail.com>
2020-12-03 15:40:35 -0300

    Colunas foram adcionadas no model user, foram inseridas validacoes e foi instalado o pundit

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_03_102219) do
-ActiveRecord::Schema.define(version: 2020_12_03_013742) do
+ActiveRecord::Schema.define(version: 2020_12_03_181334) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,6 +34,8 @@ ActiveRecord::Schema.define(version: 2020_12_03_013742) do
     t.string "checksum", null: false
     t.datetime "created_at", null: false
     t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
+
   create_table "products", force: :cascade do |t|
     t.string "category"
     t.string "subcategory"
@@ -56,6 +57,10 @@ ActiveRecord::Schema.define(version: 2020_12_03_013742) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "name", null: false
+    t.string "address", null: false
+    t.datetime "birth_date"
+    t.string "telephone"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
