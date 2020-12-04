209fca91402fbbd5d6c18cd6087c1afdf7eb2e7b
thiago almeida <thiagopalmeida@gmail.com>
2020-12-03 23:40:46 -0300

    Implementado as actions index e show junto com a view

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,4 +1,5 @@
 class ProductsController < ApplicationController
+  skip_before_action :authenticate_user!, only: %i[show index]
   before_action :set_product, only: %i[show]
 
   def new
@@ -21,10 +22,14 @@ class ProductsController < ApplicationController
   def show
   end
 
+  def index
+    @products = policy_scope(Product).order(created_at: :desc)
+  end
+
   private
 
   def product_params
-    params.require(:product).permit(:category, :subcategory, :brand, :size, :description, :price, :photos)
+    params.require(:product).permit(:category, :subcategory, :brand, :size, :description, :price, photos: [])
   end
 
   def set_product
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  resources :products, only: %i[new create show] do
+  resources :products, only: %i[new create show index] do
     resources :transactions, only: [:new, :create]
   end
 end
