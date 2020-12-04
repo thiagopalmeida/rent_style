464e971900e33ee40ab3d2deb7221e45d072d5da
Arthur Mello <arthur.mello@engenharia.ufjf.br>
2020-12-04 19:41:35 -0300

    Fixei a imagem no fundo da Home

--- app/javascript/components/navbar.js
+++ app/javascript/components/navbar.js
@@ -2,7 +2,7 @@ const initUpdateNavbarOnScroll = () => {
   const navbar = document.querySelector('.navbar-lewagon');
   if (navbar) {
     window.addEventListener('scroll', () => {
-      if (window.scrollY >= 50) {
+      if (window.scrollY >= 300) {
         // window.innerHeight) {
         navbar.classList.add('navbar-lewagon-color');
       } else {
