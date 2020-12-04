67eada211c18e62a4e2139cbc73f5ce91dd0621c
thiago almeida <thiagopalmeida@gmail.com>
2020-12-04 16:39:43 -0300

    Foi acrescentado a possibilidade de alugar a vestimenta modificando o boleano disponível no produto e liberado o about para todos

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -2,14 +2,18 @@ class TransactionsController < ApplicationController
   def new
     @product = Product.find(params[:product_id])
     @transaction = Transaction.new
+    authorize @transaction
   end
 
   def create
     @product = Product.find(params[:product_id])
     @transaction = Transaction.new(transaction_params)
     @transaction.product = @product
+    @transaction.user = current_user
+    authorize @transaction
     if @transaction.save
       redirect_to @product, notice: 'transaction was successfully created.'
+      @product.available = false
     else
       render :new
     end
