f1f22e7aab0d4a4e08bebd4416a2218c2acceb2d
Arthur Mello <arthur.mello@engenharia.ufjf.br>
2020-12-04 07:57:06 -0300

    Modificaçoes na navbar (adicionado scroll com mudança de cor) e no footer (fixado e mudança de cor dos ícones)

--- app/javascript/components/navbar.js
+++ app/javascript/components/navbar.js
@@ -3,9 +3,9 @@ const initUpdateNavbarOnScroll = () => {
   if (navbar) {
     window.addEventListener('scroll', () => {
       if (window.scrollY >= window.innerHeight) {
-        navbar.classList.add('navbar-lewagon-white');
+        navbar.classList.add('navbar-lewagon-color');
       } else {
-        navbar.classList.remove('navbar-lewagon-white');
+        navbar.classList.remove('navbar-lewagon-color');
       }
     });
   }
