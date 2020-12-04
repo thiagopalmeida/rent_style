4f6c6f15ba3b9228ab21d18f5f78d366e7dce6d7
Arthur Mello <arthur.mello@engenharia.ufjf.br>
2020-12-04 18:48:11 -0300

    Acerto dos formulários de alguns arquivos

--- app/javascript/components/navbar.js
+++ app/javascript/components/navbar.js
@@ -2,7 +2,8 @@ const initUpdateNavbarOnScroll = () => {
   const navbar = document.querySelector('.navbar-lewagon');
   if (navbar) {
     window.addEventListener('scroll', () => {
-      if (window.scrollY >= window.innerHeight) {
+      if (window.scrollY >= 50) {
+        // window.innerHeight) {
         navbar.classList.add('navbar-lewagon-color');
       } else {
         navbar.classList.remove('navbar-lewagon-color');
